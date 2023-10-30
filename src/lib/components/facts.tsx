dd2bc6edd33786a43b572b923c3c883ae5526c6f
blkluv <info@blkluv.org>
2023-10-30 17:53:22 -0400

    header change: updated

--- src/lib/components/facts.tsx
+++ src/lib/components/facts.tsx
@@ -6,45 +6,50 @@ import {
   Hero,
   Text,
   useTheme,
-} from 'components';
-import useVisible from '../../src/components/utils/use-visible'; // Update the import path here
-import { useEffect, useRef, useState } from 'react';
+} from 'components'
+import useVisible from 'components/utils/use-visibile'
+import { useEffect, useRef, useState } from 'react'
 
 export function FactItem({ amount = 0, title = '' }: { amount?: number; title: string }) {
-  const theme = useTheme();
-  const [isPlaying, setIsPlaying] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
-  const isVisible = useVisible(ref);
+  const theme = useTheme()
+  const [isPlaying, setIsPlaying] = useState(false)
+  const ref = useRef<HTMLDivElement>(null)
+  const isVisible = useVisible(ref)
 
   useEffect(() => {
     if (isVisible) {
-      setIsPlaying(true);
+      setIsPlaying(true)
     }
-  }, [isVisible]);
-
+  }, [isVisible])
   return (
     <FadeInEffect translateY="4rem" startOpacity={1} blur={10}>
       <div className="fact" ref={ref}>
         <CountUp start={0} end={amount} easing="linear" isCounting={isPlaying}>
           {({ value }) => (
-            <div className="fact-content">
-              <Text margin={0} font={'72px'} style={{ fontWeight: '500' }}>
-                {value}%
-              </Text>
-              <Text margin="8px 0 0" font={'21px'} style={{ color: theme.palette.accents_4 }}>
-                {title}
-              </Text>
-            </div>
+            <Text margin={0} font={'72px'} style={{ fontWeight: '500' }}>
+              {value}+
+            </Text>
           )}
         </CountUp>
+
+        <Text margin={0} font={'21px'} style={{ color: theme.palette.accents_4 }}>
+          {title}
+        </Text>
+        <style jsx>{`
+          .fact {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}</style>
       </div>
     </FadeInEffect>
-  );
+  )
 }
 
 export default function Facts() {
-  const theme = useTheme();
-  const ref = useRef<HTMLDivElement>(null);
+  const theme = useTheme()
+  const ref = useRef<HTMLDivElement>(null)
 
   return (
     <ContentLayout>
@@ -62,24 +67,46 @@ export default function Facts() {
               h4
               font={'clamp(24px, 3.1vw, 48px)'}
               style={{ fontWeight: '700', lineHeight: '1.2' }}>
-              We take pride in empowering creators to earn up to $9,999 per month in subscriptions, whereas platforms like TikTok and Instagram have a monthly ceiling of $99. We tax creators the least amount.
+              We take pride in empowering creators to earn up 
+              to $9,999 per month in subscriptions, whereas 
+              platforms like TikTok and Instagram have a monthly
+              ceiling of $99. We tax creators the least percentage.
             </Text>
           </FadeInEffect>
         </div>
         <Grid.Container gap={5} justify="center">
-          <Grid.Container>
+        <Grid.Container>
             <Grid xs={24} md={8} justify="center">
-              <FactItem title="NFTV" amount={20} />
+        <FactItem title="NFTV" amount={20} />
             </Grid>
+        <Grid xs={24} md={8} justify="center">
+            <FactItem title="TikTok" amount={50} />
+        </Grid>
             <Grid xs={24} md={8} justify="center">
-              <FactItem title="TikTok" amount={50} />
-            </Grid>
-            <Grid xs={24} md={8} justify="center">
-              <FactItem title="Instagram" amount={30} />
-            </Grid>
-          </Grid.Container>
-        </Grid.Container>
+            <FactItem title="Instagram" amount={30} />
+         </Grid>
+         </Grid.Container>
+         </Grid.Container>
+
+        <style jsx>{`
+          .header {
+            max-width: 860px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: 0 auto;
+            text-align: center;
+            margin-bottom: 64px;
+          }
+
+          .portfolio-list {
+            display: flex;
+            flex-direction: column;
+            gap: 60px;
+            width: 100%;
+          }
+        `}</style>
       </FadeInEffect>
     </ContentLayout>
-  );
+  )
 }
